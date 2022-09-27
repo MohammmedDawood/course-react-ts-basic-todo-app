fd2375426ab7c01ca23b27364c9a787a49a28927
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-28 00:43:50 +0200

    Working with Props & TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Todos from "./components/Todos";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Todos items={["Learn React", "learn TypeScript"]} />
+    </div>
+  );
 }
 
 export default App;
